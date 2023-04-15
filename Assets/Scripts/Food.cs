368914950290c1a5ab8a7a47d437705534a37d58
Drew Stew <drew.b.stew@gmail.com>
2023-04-15 13:11:42 +0200

    added Order and Food classes

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    List<Ingredients> ingredients = new List<Ingredients>();
+    bool isComplete = false;
+}
