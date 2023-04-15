eca2a465416440aed409480537e23ef1822e553b
Jaime Perez <jperezh3@gmail.com>
2023-04-15 11:59:54 +0200

    WIP logic for the head limiting

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,14 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
+public class HydraHead : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    public void MoveHead(Vector2 direction)
+    {
+        rigidbody.AddForce(direction);
+    }
+}
+
 public class CharacterController : MonoBehaviour
 {
     [SerializeField] private PlayerInput playerInput;
     [SerializeField] private Rigidbody2D rigidbody;
-    [SerializeField] private Vector2 movementSpeed;
-    
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float movementRadius;
+    [SerializeField] private Transform neckOrigin;
+
     private Controls controls;
 
     private void Start()
@@ -18,10 +36,31 @@ public class CharacterController : MonoBehaviour
         playerInput.user.AssociateActionsWithUser(controls);
     }
 
-    // Update is called once per frame
     private void Update()
     {
+        if (controls.Gameplay.Move.triggered)
+        {
+            Debug.Log("Movement Triggered");
+        }
+        
         var inputValue = controls.Gameplay.Move.ReadValue<Vector2>();
-        rigidbody.AddForce(inputValue * Time.deltaTime * movementSpeed);    
+        rigidbody.AddForce(inputValue * (Time.deltaTime * movementSpeed));
+        // Control limits
+        if (HasReachedLimit())
+        {
+            Debug.Log("HasReachedLimit");
+            rigidbody.AddForce(rigidbody.velocity * -1);
+        }
+    }
+
+    private bool HasReachedLimit()
+    {
+        return Vector2.Distance(neckOrigin.position, transform.position) > movementRadius;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(neckOrigin.position, movementRadius);
     }
 }
