8bcc6780efddd45fe4d89e78dbe976acf4192320
Martin Daniel Baré <martinbare92@gmail.com>
2023-04-15 13:40:46 +0200

    ingredients = ingredient

--- Assets/Scripts/Ingredient.cs
+++ Assets/Scripts/Ingredient.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ingredients : MonoBehaviour
+public class Ingredient : MonoBehaviour
 {
     public bool isCooked;
     public GameObject cookedVisual;
